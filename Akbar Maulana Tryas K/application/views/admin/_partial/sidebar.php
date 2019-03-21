f78f626cf3bf4872bf58cee6fcc5a6e570e43cba
akbarkusuma32 <akbar.kusuma3212@users.noreply.github.com>
2019-03-21 20:42:54 +0700

    Akbar Maulana Tryas K (E41171892)

---
+++ Akbar Maulana Tryas K/application/views/admin/_partial/sidebar.php
@@ -0,0 +1,43 @@
+ <!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="masterDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Master</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">-Sub Menu:</h6>
+          <a class="dropdown-item" href="barang.html">Barang</a>
+          <a class="dropdown-item" href="karyawan.html">Karyawan</a>
+          <a class="dropdown-item" href="user.html">User</a>
+          <a class="dropdown-item" href="supplier.html">Supplier</a>
+         
+        </div>
+      </li>
+       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="transkasiDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Transaksi</span></a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">-Sub Menu:</h6>
+          <a class="dropdown-item" href="penjualan.html">Penjualan</a>
+          <a class="dropdown-item" href="pembelian.html">Pembelian</a>
+         
+
+      </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="transkasiDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Laporan</span></a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">-Sub Menu:</h6>
+          <a class="dropdown-item" href="penjualan.html">Laporan Harian</a>
+          <a class="dropdown-item" href="pembelian.html">Laporan Bulanan</a>
+      </li>
+    </ul>
